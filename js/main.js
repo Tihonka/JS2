169753fd278f7c042869e09f45efa160906a6881
Aleksandra Kovalenko <al.kovalen@mail.ru>
2021-08-09 11:40:52 +0300

    исправлены конфликты

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-const products = [
-    {id: 1, title: 'Notebook', price: 2000, image: "image/notebook.jpg"},
-    {id: 2, title: 'Mouse', price: 20, image: "image/mouse.jpg"},
-    {id: 3, title: 'Keyboard', price: 200, image: "image/keyboard.jpg"},
-    {id: 4, title: 'Gamepad', price: 50, image: "image/gamepad.jpg"},
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (item) => {
-    return `<div class="product-item">
-                <h3>${item.title}</h3>
-                <img src="${item.image}">
-                <p>${item.price}</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
-const renderPage = list => {
-      document.querySelector('.products').innerHTML = list.map(item =>
-         renderProduct(item)).join('');
-   };
-
-renderPage(products);
-=======
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 const app = new Vue({
@@ -207,4 +183,3 @@ const app = new Vue({
 // }
 
 // let basket = new Cart();
->>>>>>> Stashed changes
